14b43f7df35a5971729738cb185591bf17b01e1e
Amadoubenthe <amadbenthebagnandiallo@gmail.com>
2025-02-03 20:14:31 +0000

    Add routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,7 +1,15 @@
+import { Route, Routes } from "react-router";
+import Home from "./components/home/Home";
+import About from "./components/about/About";
+
 function App() {
   return (
     <>
       <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="about" element={<About />} />
+      </Routes>
     </>
   );
 }

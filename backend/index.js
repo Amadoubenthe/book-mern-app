dc3f98553c9e95d85f7bf8494f35dac8b1cb724e
Amadoubenthe <amadbenthebagnandiallo@gmail.com>
2025-02-13 00:08:51 +0000

    Add dotenv

--- backend/index.js
+++ backend/index.js
@@ -1,7 +1,12 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+
+dotenv.config();
 const app = express();
-const port = 3000;
+
+const port = process.env.PORT || 8000;
+const dbUrl = process.env.DB_URL;
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
@@ -16,7 +21,5 @@ main()
   .catch((err) => console.log(err));
 
 async function main() {
-  await mongoose.connect("mongodb://127.0.0.1:27017/freeCodeCampDB");
-
-  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+  await mongoose.connect(dbUrl);
 }
